8f8b8644e90bd30dfdb36ce7a0e30c31037029ba
Cande <godoycandela65@gmail.com>
2026-04-20 22:44:49 -0300

    agrego baseviewmodel para manejar notificaciones de propiedades

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EstudioContableApp.ViewModels
+{
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // metodo que avisa a la interfaz que algo cambio 
+        protected void OnPropertyChanged([CallerMemberName] string? propiedad = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propiedad));
+        }
+
+        // helper que evita repetir logica en cada propiedad 
+        protected bool SetProperty<T>(ref T campo, T valor, [CallerMemberName] string? propiedad = null)
+        {
+            if (Equals(campo, valor))
+                return false;
+
+            campo = valor;
+            OnPropertyChanged(propiedad);
+            return true;
+        }
+    }
+}
