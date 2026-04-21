30947680c9533e24a9c75168baec1eeeb18b39d5
Cande <godoycandela65@gmail.com>
2026-04-20 22:23:48 -0300

    agrego servicio para consumir api de clientes con manejo de errores

---
+++ Services/ClienteService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Json;
+using EstudioContableApp.Models;
+
+namespace EstudioContableApp.Services
+{
+    public class ClienteService
+    {
+        private readonly HttpClient _httpClient = new();
+
+        // metodo que se encarga de ir a buscar los datos a la API
+
+        public async Task<List<Cliente>> ObtenerClientesAsync()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+
+                // si la respuesta fue correcta (200)
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadFromJsonAsync<List<Cliente>>();
+
+                    // si por alguna razon viene null, devuelvo lista vacia
+                    return data ?? new List<Cliente>();
+                }
+                else
+                {
+                    // esto seria un error tipo 404, 500, etc.
+                    throw new Exception($"Error HTTP: {response.StatusCode}");
+                }
+            }
+            catch (HttpRequestException)
+            {
+                // este error es mas de conexion 
+                throw new Exception("No hay conexión a internet");
+            }
+        }
+    }
+}
