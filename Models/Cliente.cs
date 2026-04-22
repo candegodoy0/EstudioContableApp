98a972ef60b4f532ee26e6c9b531fe2565bbbf28
Cande <godoycandela65@gmail.com>
2026-04-21 23:56:15 -0300

    fix: corrección deserialización nombre y agregado de vencimiento en UI

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,4 +1,6 @@
-﻿namespace EstudioContableApp.Models
+﻿using  System.Text.Json.Serialization;
+
+namespace EstudioContableApp.Models
 {
     // este modelo representa un cliente del estudio 
 
@@ -6,6 +8,7 @@
     {
         public int Id { get; set; }
 
+        [JsonPropertyName("name")]
         public string Nombre { get; set; } = string.Empty;
 
         public string Email { get; set; } = string.Empty;
