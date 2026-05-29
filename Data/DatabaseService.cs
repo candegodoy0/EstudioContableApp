09f8cee1d66748cab2338c3de25863c0e06b8c4d
Cande <godoycandela65@gmail.com>
2026-05-28 21:14:43 -0300

    creo servicio inicial de base de datos sqlite

---
+++ Data/DatabaseService.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using EstudioContableApp.Models;
+
+namespace EstudioContableApp.Data
+{
+    public class DatabaseService
+    {
+        private SQLiteAsyncConnection _database;
+
+        public async Task InitAsync()
+        {
+            if (_database != null)
+                return;
+
+            string dbPath = Path.Combine(
+                FileSystem.AppDataDirectory,
+                "estudiocontable.db3");
+
+            _database = new SQLiteAsyncConnection(dbPath);
+
+            await _database.CreateTableAsync<Cliente>();
+        }
+    }
+}
