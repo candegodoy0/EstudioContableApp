c8d85e0985633dee85ac7cea2e422d60f8febce3
Cande <godoycandela65@gmail.com>
2026-05-28 21:05:22 -0300

    agrego atributos sqlite al modelo cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,18 +1,26 @@
 ﻿using  System.Text.Json.Serialization;
+using SQLite;
 
 namespace EstudioContableApp.Models
 {
     // este modelo representa un cliente del estudio 
+    // tambien se usa como tabla local para guardar datos con SQLite
 
     public class Cliente
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
 
+        // la APIdevuelve "name", pero en la app lo usamos como Nombre
         [JsonPropertyName("name")]
+        [MaxLength(100)]
         public string Nombre { get; set; } = string.Empty;
 
+        [MaxLength(150)]
         public string Email { get; set; } = string.Empty;
 
+        // dato propio de la app para simular vencimientos del estudio contable
+        [MaxLength(50)]
         public string Vencimiento { get; set; } = "IVA - 20/05";
     }
 }
