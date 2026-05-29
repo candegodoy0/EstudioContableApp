53f5a57cab5faada7384c37ddffbaa77e420c2c8
Cande <godoycandela65@gmail.com>
2026-05-28 22:28:11 -0300

    creo implementacion de repositorio para clientes

---
+++ Repositories/ClienteRepository.cs
@@ -0,0 +1,37 @@
+﻿using EstudioContableApp.Data;
+using EstudioContableApp.Models;
+using EstudioContableApp.Services;
+
+namespace EstudioContableApp.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ClienteService _service;
+        private readonly DatabaseService _database;
+
+        public ClienteRepository()
+        {
+            _service = new ClienteService();
+            _database = new DatabaseService();
+        }
+
+        public async Task<List<Cliente>> ObtenerClientesAsync()
+        {
+            try
+            {
+                // obtengo clientes desde la API
+                var clientes = await _service.ObtenerClientesAsync();
+
+                // los guardo localmente
+                await _database.GuardarClientesAsync(clientes);
+
+                return clientes;
+            }
+            catch
+            {
+                // si falla internet, uso los datos locales
+                return await _database.ObtenerClientesAsync();
+            }
+        }
+    }
+}
